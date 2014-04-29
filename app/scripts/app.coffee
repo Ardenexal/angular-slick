81773737844f7afe43c25e36b40560619f9569a2
Alex Murray <alexmurray400@hotmail.com>
2014-04-29 12:22:29 +1000

    fixed hg-repeat issue

--- app/scripts/app.coffee
+++ app/scripts/app.coffee
@@ -1,7 +1,7 @@
 'use strict'
 
 angular.module('slick', [])
-  .directive "slick", () ->
+.directive "slick", ($timeout) ->
     restrict: "AEC"
     scope:
       accessibility: "@"
@@ -32,31 +32,32 @@ angular.module('slick', [])
       touchThreshold: "@"
       vertical: "@"
     link: (scope, element, attrs) ->
-      $(element).slick
-        accessibility: scope.accessibility isnt "false"
-        arrows: scope.arrows isnt "false"
-        autoplay: scope.autoplay is "true"
-        autoplaySpeed: if scope.autoplaySpeed? then parseInt(scope.autoplaySpeed, 10) else 3000
-        centerMode: scope.centerMode is "true"
-        centerPadding: scope.centerPadding or "50px"
-        cssEase: scope.cssEase or "ease"
-        dots: scope.dots is "true"
-        draggable: scope.draggable isnt "false"
-        easing: scope.easing or "linear"
-        fade: scope.fade is "true"
-        infinite: scope.infinite isnt "false"
-        lazyLoad: scope.lazyLoad or "ondemand"
-        onBeforeChange: scope.onBeforeChange or null
-        onAfterChange: scope.onAfterChange or null
-        onInit: scope.onInit or null
-        onReInit: scope.onReInit or null
-        pauseOnHover: scope.pauseOnHover isnt "false"
-        responsive: scope.responsive or null
-        slide: scope.slide or "div"
-        slidesToShow: if scope.slidesToShow? then parseInt(scope.slidesToShow, 10) else 1
-        slidesToScroll: if scope.slidesToScroll? then parseInt(scope.slidesToScroll, 10) else 1
-        speed: if scope.speed? then parseInt(scope.speed, 10) else 300
-        swipe: scope.swipe isnt "false"
-        touchMove: scope.touchMove isnt "false"
-        touchThreshold: if scope.touchThreshold then parseInt(scope.touchThreshold, 10) else 5
-        vertical: scope.vertical is "true"
+      $timeout ->
+        $(element).slick
+          accessibility: scope.accessibility isnt "false"
+          arrows: scope.arrows isnt "false"
+          autoplay: scope.autoplay is "true"
+          autoplaySpeed: if scope.autoplaySpeed? then parseInt(scope.autoplaySpeed, 10) else 3000
+          centerMode: scope.centerMode is "true"
+          centerPadding: scope.centerPadding or "50px"
+          cssEase: scope.cssEase or "ease"
+          dots: scope.dots is "true"
+          draggable: scope.draggable isnt "false"
+          easing: scope.easing or "linear"
+          fade: scope.fade is "true"
+          infinite: scope.infinite isnt "false"
+          lazyLoad: scope.lazyLoad or "ondemand"
+          onBeforeChange: scope.onBeforeChange or null
+          onAfterChange: scope.onAfterChange or null
+          onInit: scope.onInit or null
+          onReInit: scope.onReInit or null
+          pauseOnHover: scope.pauseOnHover isnt "false"
+          responsive: scope.responsive or null
+          slide: scope.slide or "div"
+          slidesToShow: if scope.slidesToShow? then parseInt(scope.slidesToShow, 10) else 1
+          slidesToScroll: if scope.slidesToScroll? then parseInt(scope.slidesToScroll, 10) else 1
+          speed: if scope.speed? then parseInt(scope.speed, 10) else 300
+          swipe: scope.swipe isnt "false"
+          touchMove: scope.touchMove isnt "false"
+          touchThreshold: if scope.touchThreshold then parseInt(scope.touchThreshold, 10) else 5
+          vertical: scope.vertical is "true"
--- dist/slick.js
+++ dist/slick.js
@@ -1,66 +1,71 @@
 'use strict';
-angular.module('slick', []).directive('slick', function () {
-  return {
-    restrict: 'AEC',
-    scope: {
-      accessibility: '@',
-      arrows: '@',
-      autoplay: '@',
-      autoplaySpeed: '@',
-      centerMode: '@',
-      centerPadding: '@',
-      cssEase: '@',
-      dots: '@',
-      draggable: '@',
-      easing: '@',
-      fade: '@',
-      infinite: '@',
-      lazyLoad: '@',
-      onBeforeChange: '@',
-      onAfterChange: '@',
-      onInit: '@',
-      onReInit: '@',
-      pauseOnHover: '@',
-      responsive: '@',
-      slide: '@',
-      slidesToShow: '@',
-      slidesToScroll: '@',
-      speed: '@',
-      swipe: '@',
-      touchMove: '@',
-      touchThreshold: '@',
-      vertical: '@'
-    },
-    link: function (scope, element, attrs) {
-      return $(element).slick({
-        accessibility: scope.accessibility !== 'false',
-        arrows: scope.arrows !== 'false',
-        autoplay: scope.autoplay === 'true',
-        autoplaySpeed: scope.autoplaySpeed != null ? parseInt(scope.autoplaySpeed, 10) : 3000,
-        centerMode: scope.centerMode === 'true',
-        centerPadding: scope.centerPadding || '50px',
-        cssEase: scope.cssEase || 'ease',
-        dots: scope.dots === 'true',
-        draggable: scope.draggable !== 'false',
-        easing: scope.easing || 'linear',
-        fade: scope.fade === 'true',
-        infinite: scope.infinite !== 'false',
-        lazyLoad: scope.lazyLoad || 'ondemand',
-        onBeforeChange: scope.onBeforeChange || null,
-        onAfterChange: scope.onAfterChange || null,
-        onInit: scope.onInit || null,
-        onReInit: scope.onReInit || null,
-        pauseOnHover: scope.pauseOnHover !== 'false',
-        responsive: scope.responsive || null,
-        slide: scope.slide || 'div',
-        slidesToShow: scope.slidesToShow != null ? parseInt(scope.slidesToShow, 10) : 1,
-        slidesToScroll: scope.slidesToScroll != null ? parseInt(scope.slidesToScroll, 10) : 1,
-        speed: scope.speed != null ? parseInt(scope.speed, 10) : 300,
-        swipe: scope.swipe !== 'false',
-        touchMove: scope.touchMove !== 'false',
-        touchThreshold: scope.touchThreshold ? parseInt(scope.touchThreshold, 10) : 5,
-        vertical: scope.vertical === 'true'
-      });
-    }
-  };
-});
+angular.module('slick', []).directive('slick', [
+  '$timeout',
+  function ($timeout) {
+    return {
+      restrict: 'AEC',
+      scope: {
+        accessibility: '@',
+        arrows: '@',
+        autoplay: '@',
+        autoplaySpeed: '@',
+        centerMode: '@',
+        centerPadding: '@',
+        cssEase: '@',
+        dots: '@',
+        draggable: '@',
+        easing: '@',
+        fade: '@',
+        infinite: '@',
+        lazyLoad: '@',
+        onBeforeChange: '@',
+        onAfterChange: '@',
+        onInit: '@',
+        onReInit: '@',
+        pauseOnHover: '@',
+        responsive: '@',
+        slide: '@',
+        slidesToShow: '@',
+        slidesToScroll: '@',
+        speed: '@',
+        swipe: '@',
+        touchMove: '@',
+        touchThreshold: '@',
+        vertical: '@'
+      },
+      link: function (scope, element, attrs) {
+        return $timeout(function () {
+          return $(element).slick({
+            accessibility: scope.accessibility !== 'false',
+            arrows: scope.arrows !== 'false',
+            autoplay: scope.autoplay === 'true',
+            autoplaySpeed: scope.autoplaySpeed != null ? parseInt(scope.autoplaySpeed, 10) : 3000,
+            centerMode: scope.centerMode === 'true',
+            centerPadding: scope.centerPadding || '50px',
+            cssEase: scope.cssEase || 'ease',
+            dots: scope.dots === 'true',
+            draggable: scope.draggable !== 'false',
+            easing: scope.easing || 'linear',
+            fade: scope.fade === 'true',
+            infinite: scope.infinite !== 'false',
+            lazyLoad: scope.lazyLoad || 'ondemand',
+            onBeforeChange: scope.onBeforeChange || null,
+            onAfterChange: scope.onAfterChange || null,
+            onInit: scope.onInit || null,
+            onReInit: scope.onReInit || null,
+            pauseOnHover: scope.pauseOnHover !== 'false',
+            responsive: scope.responsive || null,
+            slide: scope.slide || 'div',
+            slidesToShow: scope.slidesToShow != null ? parseInt(scope.slidesToShow, 10) : 1,
+            slidesToScroll: scope.slidesToScroll != null ? parseInt(scope.slidesToScroll, 10) : 1,
+            speed: scope.speed != null ? parseInt(scope.speed, 10) : 300,
+            swipe: scope.swipe !== 'false',
+            touchMove: scope.touchMove !== 'false',
+            touchThreshold: scope.touchThreshold ? parseInt(scope.touchThreshold, 10) : 5,
+            vertical: scope.vertical === 'true'
+          });
+        });
+      }
+    };
+  }
+]);
